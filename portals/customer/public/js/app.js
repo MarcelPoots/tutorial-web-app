d1e227e46e8c35fa8f36b3e7c4bac410552a8f30
MarcelPoots <m.poots@quicknet.nl>
2022-12-18 11:21:53 +0100

    Login mechanism part II

--- portals/customer/public/js/app.js
+++ portals/customer/public/js/app.js
@@ -1,26 +1,27 @@
 console.log('Client side javascript file is loaded!...')
 
-const weatherForm = document.querySelector('form')
+const oneForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
 const messageTwo = document.querySelector('#message-2')
 
-weatherForm.addEventListener('submit', (e) => {
+oneForm.addEventListener('submit', (e) => {
     e.preventDefault()
 
+    if (messageOne && messageTwo) {
+        messageOne.textContent = 'Loading....'
+        messageTwo.textContent = ''
 
-    messageOne.textContent = 'Loading....'
-    messageTwo.textContent = ''
-
-    fetch('/search' ).then((response) => {
-        response.json().then((data) => {
-            //console.log(data)
-            if (data.error) {
-                messageOne.textContent = data.error
-            } else {
-                messageOne.textContent = data.name
-                messageTwo.textContent = data.info
-            }
+        fetch('/search').then((response) => {
+            response.json().then((data) => {
+                //console.log(data)
+                if (data.error) {
+                    messageOne.textContent = data.error
+                } else {
+                    messageOne.textContent = data.name
+                    messageTwo.textContent = data.info
+                }
+            })
         })
-    })
+    }
 })
